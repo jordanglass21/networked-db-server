33fe3a1d13d91e8cf73bef38c87ca48f63bd72cf
ubuntu <ubuntu@cs5600sp25-vm-jglass.khoury.northeastern.edu>
2025-03-11 22:01:16 +0000

    working on listener function...

--- dbserver.c
+++ dbserver.c
@@ -25,6 +25,28 @@ void quit() {
  */
 void listener() {
 	printf("listening...\n");
+
+	// given by part 2 of assignment
+	int port = 5000;
+	// create a "listening" TCP socket which will listen for incoming connections
+	int sock = socket(AF-INET, SOCK_STREAM, 0);
+	// the address we'll bind to
+	struct sockaddr_in addr = {.sin_family = AF_INET,
+					.sin_port = htons(port),
+					.sin_addr.s_addr = 0};
+	// bind the listening socket to port 5000
+	if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+		perror("can't bind"), exit(1);
+	// tell OS to start listening on it
+    	if (listen(sock, 2) < 0)
+		perror("listen"), exit(1);
+
+	// block until we get a new connection
+	while (1) {
+        	int fd = accept(sock, NULL, NULL);
+	}
+	
+	
 }
 
 /* Reads a request from a TCP connection and performs the requested action
@@ -38,7 +60,7 @@ void handle_work(int sock_fd) {
  *  which gets an item from the queue and frees the work record.
  */
 void queue_work(int sock_fd) {
-	print("queueing work...\n");
+	printf("queueing work...\n");
 }
 
 
