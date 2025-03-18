e4142dc164232cc55e5dda5f723cb0d1ae227070
ubuntu <ubuntu@cs5600sp25-vm-jglass.khoury.northeastern.edu>
2025-03-18 01:33:01 +0000

    mutlithreaded test added

--- testing.sh
+++ testing.sh
@@ -18,9 +18,9 @@ wait
 # send quit command after 5 seconds, output FAILED if dbserver
 # exits with exit(1). [exit(0) is OK, exit(1) is failure]
 #
-##(sleep 5; echo quit) | ./dbserver 5001 || echo FAILED&
+(sleep 5; echo quit) | ./dbserver 5001 || echo FAILED&
 # give it a second to get up and running. A usleep command would be nice...
-##sleep 1
-##./dbtest --port=5001 --count=100 --threads=5
-##wait
+sleep 1
+./dbtest --port=5001 --count=100 --threads=5
+wait
 
